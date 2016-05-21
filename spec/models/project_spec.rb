b102e94463db08b3a2cad0ca9ad446b28a5048b5
raditotev <raditotev@gmail.com>
2016-05-21 11:46:43 +0100

    Add tests for project model, update validations

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Project, type: :model do
   it "has a valid factory" do
     expect(build(:project)).to be_valid
   end
+
+  describe "Model validation" do
+
+    it "is invalid without title" do
+      expect(build(:project, title: nil)).to_not be_valid
+    end
+
+    it "is invalid without descriprtio" do
+      expect(build(:project, description: nil)).to_not be_valid
+    end
+  end
 end
