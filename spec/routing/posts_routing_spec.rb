c781f7c4fd56a53d406f4eb4c18d9d17d91c88e0
raditotev <raditotev@gmail.com>
2016-05-24 18:23:09 +0100

    Update name of test helper

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -1,4 +1,4 @@
-require "rails_helper"
+require "spec_helper"
 
 RSpec.describe PostsController, type: :routing do
   describe "routing" do
--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe "posts/index", type: :view do
   before(:each) do
