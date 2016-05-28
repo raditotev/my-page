cf8c6fab01fd21de03bc3f4244219c46e5bb72a1
raditotev <raditotev@gmail.com>
2016-05-28 15:27:05 +0100

    Add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :projects
   get 'about' => 'pages#about'
   get 'portfolio' => 'pages#portfolio'
