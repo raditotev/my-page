d80512d4bd601b11a9f7e1b1915c75c27fc51520
raditotev <raditotev@gmail.com>
2016-06-03 14:53:54 +0100

    Add feature test for login

--- spec/features/admin/home_page_spec.rb
+++ spec/features/admin/home_page_spec.rb
@@ -1,21 +1,16 @@
 require 'spec_helper'
 
-feature "Admin Home page" do
-
-# given(:admin) { create(:admin) }
+feature "Home page" do
 
 before :each do
   sign_in
 end
 
-  scenario "visit home page as admin" do
-    # visit admin_path
-    # expect(page).to have_title "My Blog"
-    # expect(page).to have_css "h2", text: "Log in"
-    # fill_in 'Email', with: admin.email
-    # fill_in 'Password', with: admin.password
-    # click_button 'Log in'
+  scenario "visit" do
+    visit admin_path
+    expect(page).to have_title "My Blog"
     expect(page).to have_css "h1", text: "Admin Panel"
+    expect(page).to have_css "a", text: "Projects"
   end
 
 end
