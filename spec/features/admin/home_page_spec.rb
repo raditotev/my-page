756f9dcac1868abe44cfa7ae62c5c6db89e9016c
raditotev <raditotev@gmail.com>
2016-06-03 16:07:02 +0100

    Update admin#home feature test

--- spec/features/admin/home_page_spec.rb
+++ spec/features/admin/home_page_spec.rb
@@ -16,4 +16,24 @@ end
     expect(page).to have_css "h2", text: "Projects"
     expect(page).to have_css "h2", text: "Posts"
   end
+
+  scenario "links to projects" do
+    click_link 'Projects'
+    expect(current_path).to eq(admin_projects_path)
+  end
+
+  scenario "links to posts" do
+    click_link 'Posts'
+    expect(current_path).to eq(admin_posts_path)
+  end
+
+  scenario "links to new project" do
+    click_link 'new-project-link'
+    expect(current_path).to eq(new_project_path)
+  end
+
+  scenario "links to new post" do
+    click_link 'new-post-link'
+    expect(current_path).to eq(new_post_path)
+  end
 end
