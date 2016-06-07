bbdf35f5a13fe7b3b4c41e902dd0468d3e3f797d
raditotev <raditotev@gmail.com>
2016-06-07 11:12:45 +0100

    Update grid for posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   get 'admin/projects' => 'admin#projects'
   get 'admin/posts' => 'admin#posts'
 
+  root "posts#index"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
