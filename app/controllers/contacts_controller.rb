cd08aef2db68ddc3f92ef237eb6622af85318a32
raditotev <raditotev@gmail.com>
2016-06-09 10:24:54 +0100

    Update contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,21 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      respond_to do |format|
+        format.html { redirect_to contact_path, flash: { success: "Thank you for your message. We will contact you soon!" } }
+        format.js { flash.now[:success] = "Thank you for your message. We will contact you soon!" }
+      end
+    else
+      respond_to do |format|
+        format.html { render :new, flash.now[:error] = 'Cannot send message.' }
+        format.js { flash.now[:error] = 'Cannot send message.' }
+      end
+    end
   end
 end
