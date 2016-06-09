dc9efb56cedee632cf360f0a62885d71de66d6bd
raditotev <raditotev@gmail.com>
2016-06-09 12:26:39 +0100

    Update new contact test

--- spec/views/contacts/new.html.erb_spec.rb
+++ spec/views/contacts/new.html.erb_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe "contacts/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+   before(:each) do
+    assign(:contact, build(:contact))
+    render
+  end
+
+  it "has Contact header" do
+    assert_select "h1", "Contact"
+  end
+
+  it "renders new contact form" do
+    assert_select "form[action=?][method=?]", contacts_path, "post" do
+
+      assert_select "input#contact_name[name=?]", "contact[name]"
+      assert_select "input#contact_email[name=?]", "contact[email]"
+      assert_select "textarea#contact_message[name=?]", "contact[message]"
+    end
+  end
 end
