e62158c6415c9bcdd8c9373262e833252e50f604
raditotev <raditotev@gmail.com>
2016-06-09 12:12:07 +0100

    Update contacts controller test

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,19 +1,57 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe ContactsController, type: :controller do
 
+  let(:valid_session) { {} }
+
   describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+    before :each do
+      get :new, {}, valid_session
+    end
+    subject{response}
+
+    it{is_expected.to have_http_status(:success)}
+    it{is_expected.to render_template(:new)}
+
+    it "assigns new contact as @contact" do
+      expect(assigns(:contact)).to be_a_new(Contact)
     end
   end
 
   describe "GET #create" do
+
     it "returns http success" do
       get :create
       expect(response).to have_http_status(:success)
     end
+
+    context "with valid details" do
+      before :each do
+        post :create, {contact: attributes_for(:contact)}, valid_session
+      end
+      it "assigns a newly created contact as @contact" do
+        expect(assigns(:contact)).to be_a(Contact)
+      end
+
+      it "redirects to new contact on success" do
+        expect(response).to redirect_to new_contact_path
+      end
+    end
+
+    context "with invalid details" do
+      before :each do
+        post :create, {contact: attributes_for(:contact, name: nil, email: nil, message: nil)}, valid_session
+      end
+
+      it "assigns a newly created but unsaved contact as @contact" do
+        expect(assigns(:contact)).to be_a_new(Contact)
+      end
+
+       it "re-renders the 'new' template" do
+        expect(response).to render_template("new")
+      end
+    end
+
   end
 
 end
---
+++ spec/factories/contact.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+    factory :contact do
+      name "John Doe"
+        email "john@mail.com"
+        message "Message text"
+    end
+end
