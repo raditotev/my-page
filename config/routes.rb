076c524317a5aaa360bfd98874b5c63c1fe52395
raditotev <raditotev@gmail.com>
2016-06-10 13:50:30 +0100

    Update new contact route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   resources :projects, except: :index
-  resources "contacts", only: [:new, :create]
+  resources "contacts", only: :create
+  get 'contact' => 'contacts#new'
   get 'about' => 'pages#about'
   get 'portfolio' => 'pages#portfolio'
   get 'blog' => 'pages#blog'
