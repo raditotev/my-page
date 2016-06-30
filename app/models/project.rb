ca2158e47dbc8702d903f77ccd521027c5132849
raditotev <raditotev@gmail.com>
2016-06-30 17:26:30 +0100

    Update projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :screenshot, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :screenshot, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png", dependent: :destroy
   validates_attachment :screenshot,
   content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 
