e638cbd4a82d3107ea4c2ad2a4e8e809ce201631
raditotev <raditotev@gmail.com>
2016-10-27 10:19:21 +0100

    Remove redundant css files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,8 +7,8 @@
 //= require_directory .
 
 var wow = new WOW ({
-  offset:       75,          // distance to the element when triggering the animation (default is 0)
-  mobile:       false,       // trigger animations on mobile devices (default is true)
+  offset:  75,          // distance to the element when triggering the animation (default is 0)
+  mobile: false,       // trigger animations on mobile devices (default is true)
 });
 
  //Hide flash messages after 3s
