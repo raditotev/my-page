378dbb1651204f330e2e1fae1ddb3936537e0cfa
Radi Totev <raditotev@gmail.com>
2019-05-19 12:11:13 +0100

    Comment out depricated application config method

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module MyBlog
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w( ckeditor/* )
   end
 end
