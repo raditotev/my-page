1c3f84be0cd17f16fbc3e2b8b148d135c90283f0
Radi Totev <raditotev@gmail.com>
2019-05-26 13:24:27 +0100

    Add comment for changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
     }
   }
 
+  # Enable static asset serving and logging on Heroku
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
